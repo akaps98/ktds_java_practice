fd74f13e19a84ebbace104b45bcebf2270d0020d
Tony Kang <lgtwins0302@gmail.com>
2025-01-21 16:48:59 +0900

    1/21 contents

--- OOP/src/movie/Movie.java
+++ OOP/src/movie/Movie.java
@@ -5,5 +5,24 @@ public class Movie {
 	String title;
 	int runningTime;
 	
+	public Movie(String title, int runningTime) {
+		super();
+		this.title = title;
+		this.runningTime = runningTime;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public int getRunningTime() {
+		return runningTime;
+	}
+	public void setRunningTime(int runningTime) {
+		this.runningTime = runningTime;
+	}
+	
 	
 }
