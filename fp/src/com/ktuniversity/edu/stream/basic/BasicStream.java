1e12f7b5e3319e62e87615d8a80bda167a2671e8
Tony Kang <lgtwins0302@gmail.com>
2025-02-17 17:30:56 +0900

    2/17 contents

--- fp/src/com/ktuniversity/edu/stream/basic/BasicStream.java
+++ fp/src/com/ktuniversity/edu/stream/basic/BasicStream.java
@@ -1,14 +1,15 @@
 package com.ktuniversity.edu.stream.basic;
 
-import java.util.stream.Stream;
-
-import com.ktuniversity.edu.stream.data.Dish;
+//import java.util.stream.Stream;
+//
+//import com.ktuniversity.edu.stream.data.Dish;
 import com.ktuniversity.edu.stream.data.DishList;
 
 public class BasicStream {
 	public void printMenuStream() {
-		Stream<Dish> dishStream = DishList.get().stream();
-		dishStream.forEach(eachDish -> System.out.println(eachDish));
+//		Stream<Dish> dishStream = DishList.get().stream();
+//		dishStream.forEach(eachDish -> System.out.println(eachDish));
+		DishList.get().forEach(eachDish -> System.out.println(eachDish));
 	}
 	
 	public void printMenuNotStream() {
